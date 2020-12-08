b9a969a25645d577a2554cd0866328ff5e268c18
Natasha Seeto <tashseeto@gmail.com>
2020-12-08 13:09:53 +1000

    updated portfolio

---
+++ menu.js
@@ -0,0 +1,19 @@
+const hamburgerIcon = document.querySelector("#hamburger-icon");
+const closeIcon = document.querySelector('#close-icon');
+
+const menu = document.querySelector('.nav-bar');
+
+hamburgerIcon.onclick = () => {
+    toggleIconClasses();
+}
+
+closeIcon.onclick = () => {
+    toggleIconClasses();
+}
+
+toggleIconClasses = () => {
+    menu.classList.toggle('active');
+    hamburgerIcon.classList.toggle('inactive');
+    closeIcon.classList.toggle('inactive');
+}
+    
